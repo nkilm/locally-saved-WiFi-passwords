99198f02e32535084c1777ebde63ba43e0b53244
nkilm <nikhilmohitelhs@gmail.com>
2022-01-15 21:35:54 +0530

    store all info in the form of list

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html",host_name=main.getHost())
 
 if __name__ == "__main__":
     app.run(debug=True,port=6060)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import re
+from traceback import print_tb
 
 def getHost():
     host_name = subprocess.run('hostname',capture_output=True,shell=True).stdout.decode()
@@ -42,13 +43,19 @@ def getWiFInfo(wifi_name:str):
         return auth,password
     return "None","--None--"
 
+WIFI_INFO = []
+
+for wifi in getWifiNames():
+    security_type,key = getWiFInfo(wifi)
+    WIFI_INFO.append([wifi,security_type,key])
 
 
 if __name__ == "__main__":
     # Testing the functions defined
-
+    # print(getHost())
     # print(getWifiNames())
-    print(getWiFInfo("OnePlus_Nik"))
+    # print(getWiFInfo("OnePlus_Nik"))
     # print(getWiFInfo("Hita's Galaxy S10 Lite"))
     # print(getWiFInfo("PESU-Element Block"))
+    print(WIFI_INFO)
 
