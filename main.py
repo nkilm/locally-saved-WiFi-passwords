2a2021bfca3bdbd94e8e140c581adf60a6844c4b
nkilm <nikhilmohitelhs@gmail.com>
2022-01-15 18:23:32 +0530

    defined function(s) to do different tasks, added condition to get password only if security key is Present

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-
 import subprocess
 import re
-from unittest import result
-
-cmd_output = subprocess.run('netsh wlan show profiles',shell=True,capture_output=True)
-
-output = cmd_output.stdout.decode()
-
-saved_wifi_names = re.findall("All User Profile     :(.*)",output)
-# starts with 'All User Profile     :' (.*) means anything of length>=0
-
-result = [];
-
-for wifi_name in saved_wifi_names:
-    result.append(wifi_name.replace(" ","").replace("\r",""))
 
 
-if __name__=="__main__":
-    # print(output)
-    print(saved_wifi_names)
-    print(result)
+def getWifiNames():
+    cmd_output = subprocess.run(
+        'netsh wlan show profiles', shell=True, capture_output=True)
+    output = cmd_output.stdout.decode()
+    saved_wifi_names = re.findall("All User Profile     : (.*)", output)
+    # starts with 'All User Profile     :' (.*) means anything of length>=0
+    if(len(saved_wifi_names)==0):
+        return "No saved WiFi"
+    result = []
+    for wifi_name in saved_wifi_names:
+        result.append(wifi_name.replace("\r", ""))
+    return result
+
+def getWiFInfo(wifi_name):
+    # cmd_output = subprocess.run(['netsh','wlan','show','profiles',wifi_name,'key','=','clear'],capture_output=True)
+    cmd_output = subprocess.run(f"netsh wlan show profiles \"{wifi_name}\" key=clear",shell=True,stdout=subprocess.PIPE)
+    formatted_output = cmd_output.stdout.decode()
+    
+    security_key = re.search("Security key           : (.*)",formatted_output).group(0).replace("\r","")
+    security_key = security_key.split(": ")[1] # split and store only security status
+   
+
+    # proceed only if security key = Present
+    if(security_key == "Present"):
+        password = re.search("Key Content            : (.*)",formatted_output)
+        password = password.group(0).replace("\r","").split(": ")[1] # split and store only password
+        print(password)
+    
 
 
+if __name__ == "__main__":   
+    # Testing the functions defined
 
-
-
+    # print(getWifiNames())
+    getWiFInfo("OnePlus_Nik")
+    getWiFInfo("Hita's Galaxy S10 Lite")
+    getWiFInfo("PESU-Element Block")
 
 
 
