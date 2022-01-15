5f550dedcc7d7ba19767ab0fc4e18e467a401858
nkilm <nikhilmohitelhs@gmail.com>
2022-01-15 17:23:38 +0530

    storing wifi names in a list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import subprocess
 import re
+from unittest import result
 
 cmd_output = subprocess.run('netsh wlan show profiles',shell=True,capture_output=True)
 
@@ -9,10 +10,16 @@ output = cmd_output.stdout.decode()
 saved_wifi_names = re.findall("All User Profile     :(.*)",output)
 # starts with 'All User Profile     :' (.*) means anything of length>=0
 
+result = [];
+
+for wifi_name in saved_wifi_names:
+    result.append(wifi_name.replace(" ","").replace("\r",""))
+
 
 if __name__=="__main__":
     # print(output)
     print(saved_wifi_names)
+    print(result)
 
 
 
