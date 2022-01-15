c02a454636f177fd0801b51f7cb4bf364d555c78
nkilm <nikhilmohitelhs@gmail.com>
2022-01-15 23:58:38 +0530

    changed the website to dark bg and highlight WiFi networks with no password with reddish color

--- static/script-files/script.js
+++ static/script-files/script.js
@@ -1,1 +1,20 @@
 console.log("linked!");
+
+
+const keys = document.querySelectorAll(".key")
+
+
+keys.forEach((key)=>{
+    if(key.textContent==="--None--"){
+        const parent = key.parentNode;
+        parent.style.color = "#db7070";
+    }
+})
+
+
+
+
+
+
+
+
