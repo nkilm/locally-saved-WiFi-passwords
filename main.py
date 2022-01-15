6af632472540f16b73d19c9bb9527941eba4edce
nkilm <nikhilmohitelhs@gmail.com>
2022-01-15 16:16:27 +0530

    importing re and obtaning all saved wifi names

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 
 import subprocess
+import re
 
 cmd_output = subprocess.run('netsh wlan show profiles',shell=True,capture_output=True)
 
 output = cmd_output.stdout.decode()
 
+saved_wifi_names = re.findall("All User Profile     :(.*)",output)
+# starts with 'All User Profile     :' (.*) means anything of length>=0
+
 
 if __name__=="__main__":
-    print(output)
-
+    # print(output)
+    print(saved_wifi_names)
 
 
 
