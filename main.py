1a04de9b1e93eadbd8497ddc1a395ebc70bbd146
nkilm <nikhilmohitelhs@gmail.com>
2022-01-15 15:35:50 +0530

    importing subprocess module and getting the list of all saved passwords

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 
+import subprocess
+
+cmd_output = subprocess.run('netsh wlan show profiles',shell=True,capture_output=True)
+
+output = cmd_output.stdout.decode()
+
+
+if __name__=="__main__":
+    print(output)
 
 
 
